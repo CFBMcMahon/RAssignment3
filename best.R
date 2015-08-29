c436bdcff502201849fd9ef1a7d5992aa5e98574
Uberzaki <mc_mahon@hotmail.co.uk>
2015-08-29 11:58:14 +0100

    Fixed a bug with for statement. Currently only adds first index to indices. Building the programme up for a simple foundation.

--- best.R
+++ best.R
@@ -3,18 +3,31 @@ outcomefilename <- "outcome-of-care-measures.csv"
 #outcome can be "heart attack", "heart failure" and "pneumonia".
 
 best <- function(state, outcome) {
-	
 	data <- read.csv(outcomefilename, colClasses = "character")
 	frame <- data[which(data$State == state), ]
+	indices <- c()
+	finalcut <- NULL
 	if(!dim(frame)[1]) { stop("invalid state") }
 	if(outcome == "heart attack")
 	{
-		as.numeric(justHeartAttack)
+		finalcut <- x$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack		
 	} else if(outcome == "heart failure") {
-		
+		finalcut <- x$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure
 	} else if(outcome == "pneumonia") {
-
+		finalcut <- x$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia	
 	} else {
 		stop("invalid outcome")
 	}
+	for(i in 1:length(finalcut))
+	{
+		if(length(indices) == 0)
+		{
+			indices <- c(i)
+		} else if(TRUE) {
+		
+		} else {
+
+		}
+	}
+	indices
 }
