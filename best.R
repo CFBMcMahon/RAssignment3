635c0ac4f9e08fb7fc356a928d7ffcae01b94cd4
Uberzaki <mc_mahon@hotmail.co.uk>
2015-08-29 11:07:21 +0100

    best function now outputs error messages as requested by the specification.

--- best.R
+++ best.R
@@ -3,7 +3,18 @@ outcomefilename <- "outcome-of-care-measures.csv"
 #outcome can be "heart attack", "heart failure" and "pneumonia".
 
 best <- function(state, outcome) {
-	data <- read.csv(outcomefilename)
+	
+	data <- read.csv(outcomefilename, colClasses = "character")
 	frame <- data[which(data$State == state), ]
-	frame 
+	if(!dim(frame)[1]) { stop("invalid state") }
+	if(outcome == "heart attack")
+	{
+		as.numeric(justHeartAttack)
+	} else if(outcome == "heart failure") {
+		
+	} else if(outcome == "pneumonia") {
+
+	} else {
+		stop("invalid outcome")
+	}
 }
