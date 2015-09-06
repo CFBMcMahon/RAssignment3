2c162071fd8de6f51d0dec3634c5a10288aaf73a
Uberzaki <mc_mahon@hotmail.co.uk>
2015-09-06 11:06:04 +0100

    rankall now works as expected. Fixed indexing bug caused by using num variable instead of rank variable.

--- rankall.R
+++ rankall.R
@@ -1,24 +1,13 @@
 outcomefilename <- "outcome-of-care-measures.csv"
 #outcome can be "heart attack", "heart failure" and "pneumonia".
 rankall <- function(outcome, num = "best") {
-	rank <- NULL
 	data <- read.csv(outcomefilename, colClasses = "character")
-#	if(is.numeric(num)){
-#		rank <- num
-#	} else if(is.character(num)){
-#		if(num == "best") {
-#			rank <- 1
-#		} else if(num == "worst"){
-#			rank <- nrow(inState)
-#		}
-#	} else {
-#		stop("invalid rank")
-#	}
 	states <- split(data, data$State)		
 	hospitalsInState <- data.frame(hospital=c(), state=c())
 	for( state in states ) {
 		inState <- state
 		statetitle <- NULL
+		rank <- NULL
 		for(statename in state$State )
 		{
 			if(!is.na(statename)) { statetitle <- statename; break}
@@ -29,14 +18,28 @@ rankall <- function(outcome, num = "best") {
 			inState <- inState[which(inState$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack != "Not Available"), ]
 		#datum[which(datum$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack != "Not Available"), ]
 		} else if(outcome == "heart failure") {
-			inState <- inState[order(as.numeric(inState$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), inState$Hospital.Name) , ]
+			inState <- inState[order(as.numeric(inState$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), state$Hospital.Name) , ]
 			inState <- inState[which(inState$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure != "Not Available"), ]
 		} else if(outcome == "pneumonia") {
-			inState <- inState[order(as.numeric(inState$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia), inState$Hospital.Name), ]
+			inState <- inState[order(as.numeric(inState$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia), state$Hospital.Name), ]
 			inState <- inState[which(inState$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia != "Not Available"), ]
-		} 
-	temp <- data.frame(hospital=inState$Hospital.Name[num], state=statetitle)
-	hospitalsInState <- rbind(hospitalsInState, temp)
+		} else {
+			stop("invalid outcome") 
+		}
+		if(is.numeric(num)){
+			rank <- num
+		} else if(is.character(num)){
+			if(num == "best") {
+				rank <- 1
+			} else if(num == "worst"){
+				rank <- nrow(inState)
+			}
+		} else {
+			print("Error!")
+		}
+
+		temp <- data.frame(hospital=inState$Hospital.Name[rank], state=statetitle)
+		hospitalsInState <- rbind(hospitalsInState, temp)
 	}
 	hospitalsInState	
 }
